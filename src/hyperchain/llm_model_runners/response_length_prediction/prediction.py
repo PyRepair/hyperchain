bcbd78aec7c4025bb78951026110ec26c4523a9f
Damian Ziaber <damian.ziaber99@gmail.com>
2024-01-30 22:51:37 +0100

    Remove sklearn from requirements Add scipy requirement

--- src/hyperchain/llm_model_runners/response_length_prediction/prediction.py
+++ src/hyperchain/llm_model_runners/response_length_prediction/prediction.py
@@ -1,25 +1,28 @@
 from .task_parsing import count_paragraphs_new, get_sentences
 from .parts_of_speech import VOCABULARY
-from .svr_data import _svr_dict, _tfidf_diag
+from .svr_data import _svr_coef, _svr_intercept, _tfidf_diag
 
-from sklearn.svm import LinearSVR
-from sklearn.preprocessing import normalize
 from scipy.sparse import spdiags, hstack
 import numpy as np
 import json
 
-svr = LinearSVR()
-svr.__dict__ =  {
-    k: np.asarray(v[0], dtype=v[2]) if isinstance(v, list) and v[1] == 'np.ndarray' else v 
-    for k, v in _svr_dict.items()
-}
-
+svr_coef = np.asarray(_svr_coef, dtype=float)
 tfidf_diag = spdiags(_tfidf_diag, diags=0, m=len(_tfidf_diag), n=len(_tfidf_diag))
+
+def normalize_l2(X):
+    for i in range(X.shape[0]):
+        s = np.sqrt(sum(X.data[j] * X.data[j] for j in range(X.indptr[i], X.indptr[i + 1])))
+
+        if s == 0.0:
+            continue
+        
+        for j in range(X.indptr[i], X.indptr[i + 1]):
+            X.data[j] /= s
 
 def predict_response_length(prompts, lengths):
     X = count_paragraphs_new(get_sentences(prompts), VOCABULARY)
     X = X * tfidf_diag
-    X = normalize(X, norm='l2', copy=False)
+    normalize_l2(X)
     to_mult = np.asarray(lengths, dtype=float)[:, None]/1500
     X = hstack((X, X > 0, X.multiply(to_mult)))
-    return svr.predict(X)*1500
+    return ((X @ svr_coef) + _svr_intercept)*1500
