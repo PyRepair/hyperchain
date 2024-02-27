a8069e4ff5decf2936aeb6971bf7c0fedaad0a75
Damian Ziaber <damian.ziaber99@gmail.com>
2024-02-27 16:20:19 +0100

    Add example for function map

---
+++ examples/example_function_map.py
@@ -0,0 +1,25 @@
+from hyperchain.chain import LLMChain, FunctionChain, FunctionMapChain
+from hyperchain.llm_runners import T5ConditionalModelRunner
+from hyperchain.prompt_templates import MaskToSentinelTemplate
+
+import re
+
+async def extract_words(response):
+    return [word for word in re.split(r'[^a-zA-Z]', str(response)) if len(word) > 3]
+
+llm_chain = LLMChain(
+    template=MaskToSentinelTemplate("{masked_code}"),
+    llm_runner=T5ConditionalModelRunner(
+        model="Salesforce/codet5p-770m",
+        model_kwargs={"max_new_tokens": 100},
+    )
+)
+
+function_map_chain = FunctionMapChain([
+    ("result", extract_words, "word_list")
+])
+
+result = (llm_chain + function_map_chain).run(masked_code="def greet_user(<mask>: User):\n  print('Hi,' + <mask>)\n")
+
+print(result.result)
+print(result.word_list)
